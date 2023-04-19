12a08a80db387dc90dabe422fdfe642cf7feedbb
christianonoh <onohchibyk@gmail.com>
2023-04-19 16:34:08 +0100

    Add fetch.js

---
+++ src/modules/fetch.js
@@ -0,0 +1,65 @@
+import {
+  deployPopUp, togglePopUp,
+} from './comments.js';
+
+const Cards = document.querySelector('.container');
+
+class TVShowCards {
+  constructor() {
+    this.collection = [];
+  }
+
+  fetchCardsData = async () => {
+    const requests = [];
+
+    for (let i = 1; i <= 22; i += 1) {
+      requests.push(fetch(`https://api.tvmaze.com/shows/${i}`));
+    }
+
+    try {
+      const responses = await Promise.all(requests);
+      const data = await Promise.all(
+        responses.map((response) => response.json()),
+      );
+      this.collection = data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+
+    return this.collection;
+  };
+
+  renderCards = (cardsData) => {
+    Cards.innerHTML = '';
+    cardsData.forEach((data) => {
+      if (data.image) {
+        Cards.innerHTML += `<div class='cards'>
+              <div class='image-div'>
+                <img src='${data.image.medium}' alt='#' class='image'>
+              </div>
+              <div class='name-like'>
+                <h2 class='ch-nmae'>${data.name}</h2>
+                <span class='heart'>&#x2764;</span>
+                <span id='likeCount'>(0)</span>
+              </div>
+              <button class='comment-Btn' data-index="${data.id}">Comments</button>
+            </div>`;
+      }
+    });
+    const commentBtns = document.querySelectorAll('.comment-Btn');
+    commentBtns.forEach((commentBtn) => {
+      commentBtn.addEventListener('click', (event) => {
+        const { index } = event.target.dataset;
+        togglePopUp();
+        deployPopUp(index);
+      });
+    });
+  };
+
+  async updateCards() {
+    const cardsData = await this.fetchCardsData();
+    this.renderCards(cardsData);
+  }
+}
+
+export default TVShowCards;
