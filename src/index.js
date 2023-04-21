70ed603de3322b8c08db2460f568798fb9552786
christianonoh <onohchibyk@gmail.com>
2023-04-21 09:41:43 +0100

    Add menu features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-expressions */
 import './style.css';
 import TVShowCards from './modules/fetch.js';
+import './modules/menu.js';
 
 const tVShowCards = new TVShowCards();
 
